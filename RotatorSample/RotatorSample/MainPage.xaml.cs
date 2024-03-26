e1f6b4c47c6dc67e6a4ac3ec433acc65ddf96403
ranjithsf4081 <127499234+SivaranjithN@users.noreply.github.com>
2024-03-26 18:46:06 +0530

    Added the sample for the prepared KB.

---
+++ RotatorSample/RotatorSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace RotatorSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            DisplayAlert("Image", "Rotator Item has been tapped", "Ok");
+        }
+    }
+
+    public class RotatorModel
+    {
+        public RotatorModel(string image)
+        {
+            Image = image;
+        }
+        private string _image;
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+
+    public class RotatorViewModel
+    {
+        public RotatorViewModel()
+        {
+            ImageCollection.Add(new RotatorModel("image1.png"));
+            ImageCollection.Add(new RotatorModel("image2.png"));
+            ImageCollection.Add(new RotatorModel("image3.png"));
+            ImageCollection.Add(new RotatorModel("image4.png"));
+            ImageCollection.Add(new RotatorModel("image5.png"));
+        }
+        private List<RotatorModel> imageCollection = new List<RotatorModel>();
+        public List<RotatorModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+    }
+
+}
---
+++ RotatorSample/RotatorSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RotatorSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
